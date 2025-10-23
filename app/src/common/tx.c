a9f9a5466dfbf3dd377407277f30bfe057990110
Carlos Medeiros <carlos.medeiros@zondax.ch>
2025-10-23 18:01:47 +0100

    add apex_p device

--- app/src/common/tx.c
+++ app/src/common/tx.c
@@ -23,15 +23,12 @@
 #include "parser.h"
 #include "zxmacros.h"
 
-#if defined(TARGET_NANOS2) || defined(TARGET_STAX) || defined(TARGET_FLEX)
+#if defined(TARGET_NANOS2) || defined(TARGET_STAX) || defined(TARGET_FLEX) || defined(TARGET_APEX_P)
 #define RAM_BUFFER_SIZE 8192
 #define FLASH_BUFFER_SIZE 16384
 #elif defined(TARGET_NANOX)
 #define RAM_BUFFER_SIZE 7162
 #define FLASH_BUFFER_SIZE 16384
-#elif defined(TARGET_NANOS)
-#define RAM_BUFFER_SIZE 256
-#define FLASH_BUFFER_SIZE 8192
 #endif
 
 // Ram
@@ -42,7 +39,7 @@ typedef struct {
     uint8_t buffer[FLASH_BUFFER_SIZE];
 } storage_t;
 
-#if defined(TARGET_NANOS) || defined(TARGET_NANOX) || defined(TARGET_NANOS2) || defined(TARGET_STAX) || defined(TARGET_FLEX)
+#if defined(LEDGER_SPECIFIC)
 storage_t NV_CONST N_appdata_impl __attribute__((aligned(64)));
 #define N_appdata (*(NV_VOLATILE storage_t *)PIC(&N_appdata_impl))
 #endif
